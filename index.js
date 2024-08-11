09bc49e5e01b6c9475d4242c2f213b833c62b9ee
Xinyang Wang <136667147+mrkingseasheep@users.noreply.github.com>
2024-08-11 14:49:25 -0400

    More levels, movement, key and lock

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: once,twice,trice
-@author: 
+@author: Xinyang Wang
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-08-11
+
+wasd to move
+j to go back a level
+l to go forward
+k to reset the level
 */
 
 const player = "p"
@@ -13,6 +18,9 @@ const player2 = "q"
 const wall = "w"
 const box = "b"
 const end = "e"
+const key = "k"
+const lock = "l"
+const heart = "h"
 
 setLegend(
   [ player, bitmap`
@@ -100,9 +108,60 @@ setLegend(
 0FFFFFFFFFFFFFF0
 00FFFFFFFFFFFF00
 0000000000000000`],
+  [ key, bitmap`
+................
+...FFF6.........
+..FF.FF6........
+..FF...F........
+..F...FF........
+..FF6.FF........
+...FFFFF6.......
+.......FF6......
+........FF6.....
+.........FF6....
+........FFFF6...
+.......FF..FF6..
+...........FFF..
+..........FF....
+..........F.....
+................`],
+  [ lock, bitmap`
+0000000000000000
+0001111111111000
+0011100000011100
+01110LLLLLL01110
+01110LLLLLL01110
+0100000000000010
+010FFFFFFFFFF010
+010FFF000FFFF010
+0100FF000FFF0010
+0110FFF0FFFF0110
+0110FFF0FFFF0110
+0110FFF00FFF0110
+01100FFFFFF00110
+0011000000001100
+0001111111111000
+0000000000000000`],
+  [ heart, bitmap`
+................
+...333....333...
+..333333.33333..
+.33333333333333.
+.33333333333333.
+.33333333333333.
+.33333333333333.
+..333333333333..
+..333333333333..
+...3333333333...
+....33333333....
+....33333333....
+.....333333.....
+......3333......
+.......33.......
+................`],
 )
 
-setSolids([player, player2, wall, box])
+setSolids([player, player2, wall, box, lock, heart])
 
 let level = 0
 const levels = [
@@ -126,12 +185,43 @@ p.q..
 ww.w.
 ww.w.
 e....`,
+  map`
+p..qk
+www..
+e.l..
+ww...`,
+  map`
+w...k
+...wb
+..lwq
+pwew.`,
+  map`
+kbqwe
+..bwl
+..b..
+.p...`,
+  map`
+bb.bp
+.kbbb
+l.b..
+ebbb.`,
+  map`
+hhhhhhhhhh
+hwww.....h
+h.wl.l...h
+h.wlllb.bh
+h..l.l.b.h
+h.....b.bh
+h.q.p....h
+hhhhhhhhhh`,
 ]
 
 setMap(levels[level])
 
 setPushables({
-  [ player ]: [box]
+  [ player ]: [box],
+  [ player2]: [box],
+  [box]: [box],
 })
 
 function moveAllX(dist) {
@@ -160,19 +250,35 @@ onInput("a", () => {
 onInput("d", () => {
   moveAllX(1);
 })
+onInput("k", () => {
+  setMap(levels[level]);
+})
+onInput("j", () => {
+  if (level - 1 >= 0) {
+    setMap(levels[--level]);
+  }
+})
+onInput("l", () => {
+  if (level + 1 >= 0) {
+    setMap(levels[++level]);
+  }
+})
 
 afterInput(() => {
-  let p1 = getFirst(player);
-  let p2 = getFirst(player2);
-  let finish = getFirst(end);
+  if (tilesWith(player, key).length > 0) {
+    let key1 = getFirst(key);
+    key1.remove();
+    getFirst(lock).remove();
+  }
 
-  if (p2 !== undefined && p1.x == p2.x && p1.y == p2.y) {
+  if (tilesWith(player, player2).length > 0) {
     setMap(levels[level]);
   }
   
-  if (p1.x == finish.x && p1.y == finish.y) {
+  if (tilesWith(player, end).length > 0) {
     let nxtLvl = level + 1;
     if (nxtLvl < levels.length) {
+      level = nxtLvl;
       setMap(levels[nxtLvl]);
     } else {
       addText("you win!", { y: 4, color: color`3` });
